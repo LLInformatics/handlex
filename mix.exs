9300a9d077e733aeb447a7212c280ab3f60458d9
boksadawid <dawid.boksa@llinformatics.com>
2020-08-20 18:30:13 +0200

    add informations about package

--- mix.exs
+++ mix.exs
@@ -4,10 +4,27 @@ defmodule Handlex.MixProject do
   def project do
     [
       app: :handlex,
-      version: "1.0.0",
+      version: "0.1.0",
       elixir: "~> 1.10",
+      package: package(),
+      description: description(),
       deps: deps(),
-      name: "Handlex"
+      name: "Handlex",
+      source_url: "https://github.com/llinformatics/handlex"
+    ]
+  end
+
+  defp description() do
+    "Package with ~> and <~ pipe operators to handle :ok and :error tuples"
+  end
+
+  defp package() do
+    [
+      name: "handlex",
+      maintainers: ["Dawid Boksa"],
+      files: ~w(lib mix.exs README* LICENSE*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/llinformatics/handlex"}
     ]
   end
 
